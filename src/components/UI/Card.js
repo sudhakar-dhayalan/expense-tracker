9034ea7e8403032477d78de300fb5c7ad90ec826
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2021-12-05 12:44:15 +0530

    feat: forms and expense list is completed

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const className = "card " + props.className;
+  
+  return <div className={className}>{props.children}</div>
+}
+
+export default Card;
